7a5cdbf77ca62403cb370d8e7b76f6596f74f02e
OGShawnLee <OGShawnLee@gmail.com>
2025-12-14 21:26:32 -0600

    feat(gui): improve GUILandingAdminPage

--- src/main/java/ninco/gui/controller/GUILandingAdminPageController.java
+++ src/main/java/ninco/gui/controller/GUILandingAdminPageController.java
@@ -1,37 +1,16 @@
 package ninco.gui.controller;
 
-import ninco.gui.modal.ModalFacade;
-import ninco.gui.modal.ModalFacadeConfiguration;
-
 public class GUILandingAdminPageController extends LandingController {
   public void onClickReviewEmployeeList() {
     GUIReviewEmployeeListPageController.navigateToEmployeeListPage(getScene());
-  }
-
-  public void onClickRegisterEmployee() {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration("Register Employee", "GUIRegisterEmployeeModal")
-    );
   }
 
   public void onClickReviewProductList() {
     GUIReviewProductListPageController.navigateToProductListPage(getScene());
   }
 
-  public void onClickRegisterProduct() {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration("Register Product", "GUIRegisterProductModal")
-    );
-  }
-
   public void onClickReviewStoreList() {
     GUIReviewStoreListPageController.navigateToStoreListPage(getScene());
-  }
-
-  public void onClickRegisterStore() {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration("Register Store", "GUIRegisterStoreModal")
-    );
   }
 
   public void onClickReviewStock() {
